00ed9a5f77d3405b0b8721b29ea4f4d04154d6dd
Anushka Jachak <anushkajachak11@gmail.com>
2026-02-06 21:26:09 +0530

    Add files via upload

--- Task2_Calculator/script.js
+++ Task2_Calculator/script.js
@@ -1,1 +1,28 @@
+let display = document.getElementById("display");
 
+function append(value){
+ display.value += value;
+}
+
+function clearDisplay(){
+ display.value = "";
+}
+
+function calculate(){
+ try{
+   display.value = eval(display.value);
+ } catch{
+   display.value = "Error";
+ }
+}
+
+/* keyboard support */
+document.addEventListener("keydown", e => {
+ if("0123456789+-*/.".includes(e.key)){
+   append(e.key)
+ }
+ if(e.key === "Enter") calculate();
+ if(e.key === "Backspace") 
+   display.value = display.value.slice(0,-1)
+ if(e.key === "Escape") clearDisplay();
+});
